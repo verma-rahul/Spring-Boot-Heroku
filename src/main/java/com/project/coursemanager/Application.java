1c294a58d932472889a04d7efeca5b0918cbc2de
Rahul Verma <rahulverma@ccs.neu.edu>
2018-04-03 21:55:41 -0400

    changes to check

--- src/main/java/com/project/coursemanager/Application.java
+++ src/main/java/com/project/coursemanager/Application.java
@@ -6,13 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
