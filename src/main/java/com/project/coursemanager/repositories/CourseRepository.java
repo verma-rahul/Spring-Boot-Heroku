f86c05f946eb50b463d83a70761ef9133b080079
Rahul Verma <rahulverma@ccs.neu.edu>
2018-04-08 22:54:46 -0400

    added basic Course Level CRUD with nested inside structure(course --> module --> Section -->Widget) left

--- src/main/java/com/project/coursemanager/repositories/CourseRepository.java
+++ src/main/java/com/project/coursemanager/repositories/CourseRepository.java
@@ -2,6 +2,7 @@ package com.project.coursemanager.repositories;
 
 import com.project.coursemanager.models.Course;
 import com.project.coursemanager.models.User;
+import org.hibernate.annotations.SQLInsert;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
--- src/main/java/com/project/coursemanager/services/CourseService.java
+++ src/main/java/com/project/coursemanager/services/CourseService.java
@@ -3,12 +3,13 @@ package com.project.coursemanager.services;
 
 
 import com.project.coursemanager.models.Course;
+import com.project.coursemanager.models.Faculty;
 import com.project.coursemanager.repositories.CourseRepository;
+import com.project.coursemanager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/course")
@@ -16,9 +17,35 @@ public class CourseService {
 
     @Autowired
     CourseRepository courseRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @PostMapping("/")
-    public Course createUser(@RequestBody Course course) {
+    public Course createCourseForUser(@RequestBody Course course, @RequestParam(name="user_id", required=true) Integer userId) {
+        course.setAuthor((Faculty) userRepository.findOne(userId));
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/{courseId}")
+    public Course updateCourseName(@PathVariable("courseId") int id,@RequestBody Course course) {
+        Course newCourse=courseRepository.findOne(id);
+        newCourse.setName(course.getName());
+        return courseRepository.save(newCourse);
+    }
+
+    @GetMapping("/")
+    public List<Course> getAllCourses(@RequestParam(name="user_id", required=true) Integer userId) {
+        return ((Faculty) userRepository.findOne(userId)).getAuthoredCourses();
+
+    }
+    @GetMapping("/{courseId}")
+    public Course getCourseById(@PathVariable("courseId") int id) {
+        return courseRepository.findOne(id);
+
+    }
+
+    @DeleteMapping ("/{courseId}")
+    public void deleteCourseById(@PathVariable("courseId") int id) {
+        courseRepository.delete(id);
     }
 }
