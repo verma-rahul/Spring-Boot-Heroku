e4ee882261ddf2879ac2faa5b6d228e23c9133d0
Rahul Verma <rahulverma@ccs.neu.edu>
2018-04-14 21:18:09 -0400

    returning faculty as json as demo object

--- src/main/java/com/project/coursemanager/services/Demo.java
+++ src/main/java/com/project/coursemanager/services/Demo.java
@@ -1,4 +1,6 @@
 package com.project.coursemanager.services;
+import com.project.coursemanager.models.Faculty;
+import com.project.coursemanager.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class Demo {
 
     @GetMapping("/")
-    public String findAllUsers() {
-       return "Hello from Spring Boot Server!";
+    public User demoFaculty() {
+       return new Faculty("Jose-An", "jose-password", "Jose", "Annunziato","123 A",true);
     }
 }
